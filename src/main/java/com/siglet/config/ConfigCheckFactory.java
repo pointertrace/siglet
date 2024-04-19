353b66f5e195e77e8042d00d3d44a2a01e935a14
osvaldopina <osvaldo.pina@gmail.com>
2024-04-19 11:20:17 -0300

    sonar

--- src/main/java/com/siglet/config/ConfigCheckFactory.java
+++ src/main/java/com/siglet/config/ConfigCheckFactory.java
@@ -15,6 +15,24 @@ import com.siglet.spanlet.traceaggregator.TraceAggregatorTypes;
 import static com.siglet.config.parser.schema.SchemaFactory.*;
 
 public class ConfigCheckFactory {
+    public static final String GRPC_PROP = "grpc";
+    public static final String ADDRESS_PROP = "address";
+    public static final String DEBUG_PROP = "debug";
+    public static final String SPANLET_PROP = "spanlet";
+    public static final String TO_PROP = "to";
+    public static final String TYPE_PROP = "type";
+    public static final String CONFIG_PROP = "config";
+    public static final String TRACELET_PROP = "tracelet";
+    public static final String TRACE_AGGREGATOR_PROP = "trace-aggregator";
+    public static final String TRACE_PROP = "trace";
+    public static final String FROM_PROP = "from";
+    public static final String START_PROP = "start";
+    public static final String PIPELINE_PROP = "pipeline";
+    public static final String RECEIVERS_PROP = "receivers";
+    public static final String EXPORTERS_PROP = "exporters";
+    public static final String PIPELINES_PROP = "pipelines";
+
+//    private static final String 
 
 
     public static NodeChecker receiversChecker() {
@@ -22,12 +40,12 @@ public class ConfigCheckFactory {
         return array(
                 alternative(
                         strictObject(GrpcReceiverItem::new,
-                                requiredProperty(GrpcReceiverItem::setName, "grpc", text()),
-                                requiredProperty(GrpcReceiverItem::setAddress, "address", text(inetSocketAddress()))
+                                requiredProperty(GrpcReceiverItem::setName, GRPC_PROP, text()),
+                                requiredProperty(GrpcReceiverItem::setAddress, ADDRESS_PROP, text(inetSocketAddress()))
                         ),
                         strictObject(DebugReceiverItem::new,
-                                requiredProperty(DebugReceiverItem::setName, "debug", text()),
-                                requiredProperty(DebugReceiverItem::setAddress, "address", text())
+                                requiredProperty(DebugReceiverItem::setName, DEBUG_PROP, text()),
+                                requiredProperty(DebugReceiverItem::setAddress, ADDRESS_PROP, text())
                         )
                 )
         );
@@ -39,12 +57,12 @@ public class ConfigCheckFactory {
         return array(
                 alternative(
                         strictObject(GrpcExporterItem::new,
-                                requiredProperty(GrpcExporterItem::setName, "grpc", text()),
-                                requiredProperty(GrpcExporterItem::setAddress, "address", text(inetSocketAddress()))
+                                requiredProperty(GrpcExporterItem::setName, GRPC_PROP, text()),
+                                requiredProperty(GrpcExporterItem::setAddress, ADDRESS_PROP, text(inetSocketAddress()))
                         ),
                         strictObject(DebugExporterItem::new,
-                                requiredProperty(DebugExporterItem::setName, "debug", text()),
-                                requiredProperty(DebugExporterItem::setAddress, "address", text()))
+                                requiredProperty(DebugExporterItem::setName, DEBUG_PROP, text()),
+                                requiredProperty(DebugExporterItem::setAddress, ADDRESS_PROP, text()))
                 )
         );
     }
@@ -53,52 +71,52 @@ public class ConfigCheckFactory {
 
         return alternative(
                 strictObject(SpanletItem::new,
-                        requiredProperty(SpanletItem::setName, "spanlet", text()),
-                        alternativeRequiredProperty("to",
-                                requiredProperty(SpanletItem::setTo, "to", array(text())),
-                                requiredProperty(SpanletItem::setToSingleValue, "to", text())),
-                        requiredProperty(SpanletItem::setType, "type", text()),
-                        requiredDynamicProperty("config",
+                        requiredProperty(SpanletItem::setName, SPANLET_PROP, text()),
+                        alternativeRequiredProperty(TO_PROP,
+                                requiredProperty(SpanletItem::setTo, TO_PROP, array(text())),
+                                requiredProperty(SpanletItem::setToSingleValue, TO_PROP, text())),
+                        requiredProperty(SpanletItem::setType, TYPE_PROP, text()),
+                        requiredDynamicProperty(CONFIG_PROP,
                                 new SpanletCheckerDiscriminator(new SpanletTypes()))),
                 strictObject(TraceletItem::new,
-                        requiredProperty(TraceletItem::setName, "tracelet", text()),
-                        alternativeRequiredProperty("to",
-                                requiredProperty(TraceletItem::setTo, "to", array(text())),
-                                requiredProperty(TraceletItem::setToSingleValue, "to", text())),
-                        requiredProperty(TraceletItem::setType, "type", text()),
-                        requiredDynamicProperty("config",
+                        requiredProperty(TraceletItem::setName, TRACELET_PROP, text()),
+                        alternativeRequiredProperty(TO_PROP,
+                                requiredProperty(TraceletItem::setTo, TO_PROP, array(text())),
+                                requiredProperty(TraceletItem::setToSingleValue, TO_PROP, text())),
+                        requiredProperty(TraceletItem::setType, TYPE_PROP, text()),
+                        requiredDynamicProperty(CONFIG_PROP,
                                 new TraceletCheckerDiscriminator(new TraceletTypes()))),
                 strictObject(TraceAggregatorItem::new,
-                        requiredProperty(TraceAggregatorItem::setName, "trace-aggregator", text()),
-                        alternativeRequiredProperty("to",
-                                requiredProperty(TraceAggregatorItem::setTo, "to", array(text())),
-                                requiredProperty(TraceAggregatorItem::setToSingleValue, "to", text())),
-                        property(TraceAggregatorItem::setType, "type", false, text()),
-                        requiredDynamicProperty("config",
+                        requiredProperty(TraceAggregatorItem::setName, TRACE_AGGREGATOR_PROP, text()),
+                        alternativeRequiredProperty(TO_PROP,
+                                requiredProperty(TraceAggregatorItem::setTo, TO_PROP, array(text())),
+                                requiredProperty(TraceAggregatorItem::setToSingleValue, TO_PROP, text())),
+                        property(TraceAggregatorItem::setType, TYPE_PROP, false, text()),
+                        requiredDynamicProperty(CONFIG_PROP,
                                 new TraceAggregatorCheckerDiscriminator(new TraceAggregatorTypes())))
         );
     }
 
     public static NodeChecker tracePipelineChecker() {
         return array(strictObject(TracePipelineItem::new,
-                requiredProperty(TracePipelineItem::setName, "trace", text()),
-                alternativeRequiredProperty("from",
-                        requiredProperty(TracePipelineItem::setFrom, "from", array(text())),
-                        requiredProperty(TracePipelineItem::setFromSingleValue, "from", text())),
-                alternativeRequiredProperty("start",
-                        requiredProperty(TracePipelineItem::setStart, "start", array(text())),
-                        requiredProperty(TracePipelineItem::setStartSingleValue, "start", text())),
-                requiredProperty(TracePipelineItem::setProcessors, "pipeline",
+                requiredProperty(TracePipelineItem::setName, TRACE_PROP, text()),
+                alternativeRequiredProperty(FROM_PROP,
+                        requiredProperty(TracePipelineItem::setFrom, FROM_PROP, array(text())),
+                        requiredProperty(TracePipelineItem::setFromSingleValue, FROM_PROP, text())),
+                alternativeRequiredProperty(START_PROP,
+                        requiredProperty(TracePipelineItem::setStart, START_PROP, array(text())),
+                        requiredProperty(TracePipelineItem::setStartSingleValue, START_PROP, text())),
+                requiredProperty(TracePipelineItem::setProcessors, PIPELINE_PROP,
                         array(spanletChecker()))));
     }
 
     public static NodeChecker globalConfigChecker() {
         return strictObject(ConfigItem::new,
-                requiredProperty(ConfigItem::setReceivers, "receivers",
+                requiredProperty(ConfigItem::setReceivers, RECEIVERS_PROP,
                         receiversChecker()),
-                requiredProperty(ConfigItem::setExporters, "exporters",
+                requiredProperty(ConfigItem::setExporters, EXPORTERS_PROP,
                         grpcExportersChecker()),
-                requiredProperty(ConfigItem::setPipelines, "pipelines",
+                requiredProperty(ConfigItem::setPipelines, PIPELINES_PROP,
                         tracePipelineChecker()));
     }
 }
