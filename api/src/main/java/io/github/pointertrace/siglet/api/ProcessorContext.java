8ab5b939149967ed4f7520b0a87f8cb6b4080fd9
osvaldopina <osvaldo.pina@gmail.com>
2025-09-05 16:06:50 -0300

    simplification of context attributes

--- api/src/main/java/io/github/pointertrace/siglet/api/ProcessorContext.java
+++ api/src/main/java/io/github/pointertrace/siglet/api/ProcessorContext.java
@@ -1,21 +1,11 @@
 package io.github.pointertrace.siglet.api;
 
-import java.util.Collection;
-import java.util.function.BiFunction;
+import java.util.concurrent.ConcurrentMap;
 
 public interface ProcessorContext<T> {
 
-    Object getAttribute(String name);
-
-    <E> E getAttribute(String name, Class<E> type);
-
-    Collection<String> getAttributeNames();
-
-    void setAttribute(String name, Object value);
-
-    Object mergeAttributeValue(String name, Object value, BiFunction<Object, Object, Object> merger);
-
-    <E> E mergeAttributeValue(String name, E value, BiFunction<E, E, E> merger, Class<E> type);
+    ConcurrentMap<String, Object> getAttributes();
 
     T getConfig();
+
 }
