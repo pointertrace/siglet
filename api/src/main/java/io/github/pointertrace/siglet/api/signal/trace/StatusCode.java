59ce4958800b5c46e73cac9207e41bcb1c9964c8
osvaldopina <osvaldo.pina@gmail.com>
2026-02-18 15:47:07 -0300

    working on api javadocs

--- api/src/main/java/io/github/pointertrace/siglet/api/signal/trace/StatusCode.java
+++ api/src/main/java/io/github/pointertrace/siglet/api/signal/trace/StatusCode.java
@@ -4,7 +4,6 @@ package io.github.pointertrace.siglet.api.signal.trace;
  * Defines the status codes for a span.
  */
 public enum StatusCode {
-
     /**
      * Indicates that the span status is unset.
      */
