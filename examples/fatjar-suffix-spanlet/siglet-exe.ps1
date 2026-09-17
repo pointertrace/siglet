07a42ec63e311d0e39b97133dc981bae51d1439e
osvaldopina <osvaldo.pina@gmail.com>
2026-09-17 15:09:37 -0300

    build

--- examples/fatjar-suffix-spanlet/siglet-exe.ps1
+++
@@ -1,4 +0,0 @@
-java `
-  -jar "C:\Users\deinf.osvaldo\desenv\projetos\new-projects\siglet\implementation\target\siglet-0.0.1-SNAPSHOT.jar" `
-  --config=outro-siglet-config.yaml `
-  --siglet=target/fatjar-suffix-spanlet.jar
